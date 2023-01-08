5ba46f0d103e2d5ed7fad3f7711f5cd0f86ff422
Jaque Laurenti <jaquelinelaurenti@gmail.com>
2023-01-08 00:51:33 -0300

    fix: adicionando validações no script

--- lambda-dynamo-sns/index.js
+++ lambda-dynamo-sns/index.js
@@ -1,102 +1,129 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
+exports.handler = (event, context, callback) => {
 
-exports.handler = function (event, context, callback) {
+  if (!event || !event.id || !event.message) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Missing required fields: id and message' }),
+    };
+  }
+  // cria um objeto do tipo DocumentClient, que é um cliente especializado
+  // para trabalhar com o DynamoDB
+  const docClient = new AWS.DynamoDB.DocumentClient();
 
-  console.log(event);
+  // define os parâmetros para a operação de salvar os dados
+  const params = {
+    TableName: 'lambda-sns-dynamo', // nome da tabela
+    Item: {
+      id: event.id, // ID do item a ser salvo
+      message: event.message, // mensagem a ser salva
+    },
+  };
+
+  // tenta salvar os dados no DynamoDB
+  try {
+    const result = docClient.put(params).promise();
+    if (!result) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Error saving data to DynamoDB' }),
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error saving data to DynamoDB' }),
+    };
+  }
+
+  // Cria uma conexão com o SNS
+  const sns = new AWS.SNS();
   // determina o nome do tópico
   const topicName = "topico-dynamo-sns-create";
 
-  // Cria uma conexão com o DynamoDB
-  const dynamodb = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
-
-  // Obtenha o corpo da solicitação
-  var body = event;
-
-  console.log("body");
-  console.log(event.body)
-
-  // Define os parâmetros para a operação de inserção de item
-  const params = {
-    TableName: "lambda-sns-dynamo",
-    Item: body,
+  // Define os parâmetros para a operação de criação de tópico
+  const paramsTopic = {
+    Name: topicName,
   };
 
-
-  // Insere o registro na tabela
-  dynamodb.put(params, (error) => {
-    if (error) {
-      console.log("Deu Erro")
-      console.log(error)
-      return;
-    }
-
-    // Cria uma conexão com o SNS
-    const sns = new AWS.SNS();
-
-    // Define os parâmetros para a operação de criação de tópico
-    const params = {
-      Name: topicName,
-    };
-
-    // Cria o tópico SNS
-    sns.createTopic(params, (error, data) => {
+  // Cria o tópico SNS
+  try {
+    sns.createTopic(paramsTopic, (error, data) => {
       if (error) {
         console.error(error);
         return;
       }
-
       // Obtém o Amazon Resource Name (ARN) do tópico
-      const topicArn = data.TopicArn;
+      topicArn = data.TopicArn;
+    });
 
-      // Envia uma mensagem para o tópico SNS informando que o registro foi criado no DynamoDB
-      const params = {
-        Message: `O registro ${body.id} foi inserido no Dynamo`,
-        Subject: "Novo registro no DynamoDB",
-        TopicArn: topicArn,
-      };
+    // Publica mensagem
+    // Envia uma mensagem para o tópico SNS informando que o registro foi criado no DynamoDB
+    const paramsMessage = {
+      Message: `A mensagem ${event.message} foi inserida no Dynamo 
+        tabela: lambda-sns-dynamo id: ${event.id}`,
+      Subject: "Novo registro no DynamoDB",
+      TopicArn: topicArn,
+    };
 
-
-      // Publica o tópico
-      sns.publish(params, (error) => {
+    // Publica o tópico
+    try {
+      sns.publish(paramsMessage, (error) => {
         if (error) {
           console.log(error)
           console.error(error);
           return;
         }
       });
-
-
-      // Defina o ID do tópico e a URL de retorno
-      var endpoint = 'arn:aws:lambda:us-east-1:567374489415:function:lambda-sns-signature';
-
-      // Crie a assinatura
-      var paramsSubscribe = {
-        Protocol: 'LAMBDA',
-        TopicArn: topicArn,
-        Endpoint: endpoint
+    }
+    catch(error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Error a publish Topic SNS' }),
       };
+    }
 
+    // Assina o tópico
+    // Defina o ID do tópico e a URL de retorno
+    var endpoint = 'arn:aws:lambda:us-east-1:567374489415:function:lambda-sns-signature';
 
-      sns.subscribe(paramsSubscribe, function (err, data) {
-        if (err) {
+    // Crie a assinatura
+    var paramsSubscribe = {
+      Protocol: 'LAMBDA',
+      TopicArn: topicArn,
+      Endpoint: endpoint
+    };
+
+    try {
+      sns.subscribe(paramsSubscribe, function (error, data) {
+        if (error) {
           console.log("Deu erro na assinatura");
-          console.log(err);
-          console.log(err, err.stack);
+          console.log(error);
+          console.log(error, error.stack);
         } else {
           console.log("Fez a assinatura");
           console.log(data);
           // Envie uma resposta de sucesso para o cliente
           callback(null, {
             statusCode: 200,
-            body: `Informação adiconada no Dynamo e assinada no tópico! \n 
-              Assinatura ID: ${data.ResponseMetadata.RequestId} \n
-              ARN Tópico: ${data.SubscriptionArn}\n`
+            body: `Sua mensagem foi adicionada no Dynamo, Publicada no Tópico e Assinada ID de Assinatura:  \n 
+              ${data.ResponseMetadata.RequestId}`
           });
         }
       });
-    });
-  });
+    }
+    catch(error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Error a subscribe Topic SNS' }),
+      };
+    }
 
-}
-
-
+  }
+  catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error create Topic SNS' }),
+    };
+  }
+} // end handle
